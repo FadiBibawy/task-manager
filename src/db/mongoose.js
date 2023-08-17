64e9b0be47f25820b23cccad74c69eaf894b21f7
FadiBibawy <eng.fadimichel@gmail.com>
2023-08-17 16:13:49 +0200

    created user and task models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,45 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://0.0.0.0:27017/")
+  .connect("mongodb://127.0.0.1:27017/task-manager-api")
   .then(() => console.log("Connected!"));
+
+const userSchema = new mongoose.Schema({ name: String, age: Number });
+const User = mongoose.model("User", userSchema);
+
+const taskSchema = new mongoose.Schema({
+  description: String,
+  completed: { type: Boolean, default: false },
+});
+const Task = mongoose.model("Task", taskSchema);
+// mongoose.connection.dropCollection("tanks");
+
+const fadi = new User({ name: "Fadi", age: "30" });
+const task = new Task({ description: "First taks" });
+
+task
+  .save()
+  .then(() => {
+    console.log(task);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+// fadi
+//   .save()
+//   .then(() => {
+//     console.log(fadi);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+User.find().then((users) => {
+  console.log(users);
+});
+
+// const tanks = Tank.find({ name: "the second" });
+// tanks.forEach((element) => {
+//   console.log(element);
+// });
+// console.log(JSON.stringify(tanks));
