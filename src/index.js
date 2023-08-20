240bba1ed6acdd0bef10af6a895cc2944a44a7cb
FadiBibawy <eng.fadimichel@gmail.com>
2023-08-20 23:47:06 +0200

    refactoring tasks controller using async functions, try catch blocks, and await

--- src/index.js
+++ src/index.js
@@ -11,89 +11,145 @@ app.get("/", function (req, res) {
 });
 
 // Read all tasks
-app.get("/tasks", (req, res) => {
+app.get("/tasks", async (req, res) => {
   // await res.send(tasks);
-  Task.find()
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });
+
+  try {
+    const tasks = await Task.find();
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+
+  // Task.find()
+  //   .then((tasks) => {
+  //     res.send(tasks);
+  //   })
+  //   .catch((e) => {
+  //     res.status(500).send(e);
+  //   });
 });
 
 // Read Task
-app.get("/tasks/:id", (req, res) => {
-  Task.findById(req.params.id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.send(task);
-    })
-    .catch((e) => {
-      if (e.name == "CastError") {
-        return res.status(404).send(e);
-      }
-      res.status(500).send(e);
-    });
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).send(e);
+    }
+    res.send(task);
+  } catch (e) {
+    if (e.name == "CastError") {
+      return res.status(404).send(e);
+    }
+    res.status(500).send(e);
+  }
+
+  // Task.findById(req.params.id)
+  //   .then((task) => {
+  //     if (!task) {
+  //       return res.status(404).send();
+  //     }
+  //     res.send(task);
+  //   })
+  //   .catch((e) => {
+  //     if (e.name == "CastError") {
+  //       return res.status(404).send(e);
+  //     }
+  //     res.status(500).send(e);
+  //   });
 });
 
 // create task
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   // await res.send(tasks);
-  const task = new Task(req.body);
 
-  task
-    .save()
-    .then((task) => {
-      res.status(201).send(task);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+  // const task = new Task(req.body);
+  // task
+  //   .save()
+  //   .then((task) => {
+  //     res.status(201).send(task);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e);
+  //   });
 });
 
 // Read all users
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+
+  // User.find({})
+  //   .then((users) => {
+  //     res.send(users);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e);
+  //   });
 });
 
 // Read User
-app.get("/users/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch((e) => {
-      if (e.name == "CastError") {
-        return res.status(404).send();
-      }
-      res.status(500).send(e);
-    });
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    if (e.name == "CastError") {
+      return res.status(404).send();
+    }
+    res.status(500).send(e);
+  }
+
+  // User.findById(req.params.id)
+  //   .then((user) => {
+  //     if (!user) {
+  //       return res.status(404).send();
+  //     }
+  //     res.send(user);
+  //   })
+  //   .catch((e) => {
+  //     if (e.name == "CastError") {
+  //       return res.status(404).send();
+  //     }
+  //     res.status(500).send(e);
+  //   });
 });
 
 // create user
-app.post("/users", (req, res) => {
-  const user = new User(req.body);
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  // const user = new User(req.body);
+  // user
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e);
+  //   });
 });
 
 app.delete("/users/:id", (req, res) => {
